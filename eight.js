43fdce4db0f1f9a22dd14ab588799def0c53a3eb
Oh seung min <ohmin@gmail.com>
2024-03-01 13:12:24 +0900

    자바스크립트에서 식별자로 사용될 수 있는 예제 3가지

---
+++ eight.js
@@ -0,0 +1,17 @@
+// 문제 8: 자바스크립트에서 식별자로 사용될 수 있는 예를 3가지 제시하세요.
+// 식별자는 변수, 함수, 속성 접근이 있다.
+// 속성은 객체 혹은 배열에 접근 하는 방법이다.
+
+var one = 1;
+let two = 2;
+const number = 3;
+
+function add(x, y) {
+  return x + y;
+}
+
+const obj = {
+  id : "ohMin"
+}
+
+const list = [1, 2]
