1baaece3a71ad32f7049198e791fb6f09fff77ad
Oh seung min <ohmin@gmail.com>
2024-03-08 21:46:02 +0900

    대소문자를 구분하는 예를 작성하는 문제

---
+++ thirteen.js
@@ -0,0 +1,9 @@
+// 문제 13 : 자바스크립트에서 대소문자를 구분하는 예를 들어 보세요.
+
+let value = "Hello"
+let text = "World"
+
+console.log(value.toUpperCase())
+console.log(value.toLowerCase())
+console.log(text.toUpperCase())
+console.log(text.toLowerCase())
