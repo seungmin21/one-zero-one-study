e44950ee20280d707aa3ab9da41801caf7e07f8a
Oh seung min <ohmin@gmail.com>
2024-03-09 19:40:56 +0900

    javascript의 데이터 타입을 나열하는 문제

---
+++ fourteen.js
@@ -0,0 +1,18 @@
+// 문제 14: 자바스크립트에서 사용하는 데이터 타입들을 나열 하세요.
+
+// number
+let number = 1
+let x = 2
+const result = number < x
+// string
+let string = "문장"
+// null
+var foo = null
+// undefined
+x = undefined
+// object
+const obj = {
+  id : "홍길동"
+}
+const sym1 = Symbol();
+const sym2 = Symbol("foo");
