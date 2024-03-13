4454a33170e1c01251da766ca232c35a0c50fc1c
Oh seung min <ohmin@gmail.com>
2024-03-13 23:12:41 +0900

    typeof 연산자를 사용해 변수와 타입을 확인하는 예제 코드를 만드는 문제

---
+++ sixteen.js
@@ -0,0 +1,11 @@
+// 문제 16 : typeof 연산자를 사용해 변수와 타입을 확인하는예제 코드를 작성하세요.
+
+let str = "문장";
+console.log(typeof(sty))
+console.log(typeof(str))
+
+const num = 1
+console.log(typeof(num))
+
+const obj = { name : "min"}
+console.log(typeof(obj))
