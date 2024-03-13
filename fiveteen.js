ef5ab96498c18db979897def17c2d3023a007ba8
Oh seung min <ohmin@gmail.com>
2024-03-13 23:10:26 +0900

    조건문에서 true와 false는 truthy와 falsy로 불린다. 그것의 예를 드는 문제

---
+++ fiveteen.js
@@ -0,0 +1,15 @@
+// 문제 15: 조건문에서 truthy와 falsy라는 개념이 무엇인가요? 각각의 예를 들어보세요.
+
+let num = 1;
+
+// true === truthy
+if (num === 1) {
+  console.log("정답")
+}
+
+// false === falsy
+if (num === 0) {
+  console.log("정답")
+} else {
+  console.log("실패")
+}
