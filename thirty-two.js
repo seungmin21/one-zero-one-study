80043566973776aa3823a134e16a9c8b7c773001
Oh seung min <ohmin@gmail.com>
2024-03-31 11:28:07 +0900

    객체의 속성에 접근하는 두 가지 방법 로직 작성 문제 수정

--- thirty-two.js
+++ thirty-two.js
@@ -1,10 +1,8 @@
 // 문제 32 : 객체의 속성에 접근하는 두 가지 방법을 코드로 작성하세요.
 
-const obj = {}
-// 점표기법으로 속성을 추가
-obj.name = "대덕대로"
-obj.age = 182
-// 대괄호 표기법
-obj['sub'] = '컴퓨터'
+const obj = { name : "대덕대로", age : 182, sub : "컴퓨터" }
 
-console.log(obj)
+// 점표기법
+console.log(obj.name)
+// 대괄호 표기법
+console.log(obj["sub"])
