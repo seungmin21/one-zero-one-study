95f1091f74bfcd6f438df5bdd5959d9a25b5d462
Oh seung min <ohmin@gmail.com>
2024-03-31 11:29:01 +0900

    정정 : 객체의 속성을 삭제하는 코드 -> 객체에 새로운 속성을 동적으로 추가하는 코드를 작성하는 문제

--- thirty-three.js
+++ thirty-three.js
@@ -1,8 +1,11 @@
-// 문제 33 : 객체의 속성을 삭제하는 코드를 작성하세요.
+// 문제 33 : 객체에 새로운 속성을 동적으로 추가하는 코드를 작성하세요.
 
-const obj = { name : "대덕대로", age : 182, sub : "컴퓨터" }
-delete obj.sub;
+
+const obj = {}
+// 점표기법으로 속성을 추가
+obj.name = "대덕대로"
+obj.age = 182
+// 대괄호 표기법
+obj['sub'] = '컴퓨터'
+
 console.log(obj)
-
-const { age, ...rest} = obj;
-console.log(rest)
