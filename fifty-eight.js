5bb03f54c82d844b514da827e87c47a7c95a9a15
Oh seung min <ohmin@gmail.com>
2024-04-25 13:42:51 +0900

    모듈에서 여러 개의 항목을 내보내는 코드를 작성하는 문제

---
+++ fifty-eight.js
@@ -0,0 +1,11 @@
+// 문제 58 : 모듈에서 여러 개의 항목을 내보내는 코드를 작성하세요.
+
+export function add(key, value) {
+  return key + value;
+}
+
+export function subtract(key, value) {
+  return key - value;
+}
+
+export const index = 0
