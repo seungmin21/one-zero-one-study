2ce5b572380006967f8189cae4112ce8f5558776
Oh seung min <ohmin@gmail.com>
2024-05-01 19:34:49 +0900

    entries 메서드는 JSON 문자열로 변환 혹은 객체로 변환에 적합하지 않다.

---
+++ sixty-four.js
@@ -0,0 +1,8 @@
+// 문제 64 : set 객체를 사용해 JSON 문자열을 객체로 변환하는 코드를 작성하세요.
+
+const setting = new Set();
+setting.add(28)
+setting.add('홍길동')
+const iterator = setting.entries();
+
+console.log(JSON.parse(JSON.stringify(iterator)));
