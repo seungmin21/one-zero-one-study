2519a827f5e690eefabf3b2ec9fe16bc48a831fa
Oh seung min <ohmin@gmail.com>
2024-05-26 15:39:02 +0900

    Javascript에서 문자열을 숫자로 변환하는 방법을 예로 들어보는 문제

---
+++ eighty-eight.js
@@ -0,0 +1,9 @@
+// 문제 88 : Javascript에서 문자열을 숫자로 변환하는 방법을 예로 들어보세요.
+
+const string = "123"
+const str = "456"
+
+console.log(parseInt(string))
+console.log(typeof(parseInt(string)))
+console.log(Number(str))
+console.log(typeof(Number(str)))
