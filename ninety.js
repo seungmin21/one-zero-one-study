9b6eac8725e0682e7e30419923b387b265c867e2
Oh seung min <ohmin@gmail.com>
2024-06-07 20:01:26 +0900

    eighty-one~ninety 폴더로 이동

--- ninety.js
+++
@@ -1,7 +0,0 @@
-// 문제 90 : JavaScript에서 map, filter, reduce 함수를 사용하는 예를 들어보세요.
-
-const list = ["list", "array", "object", "string", "number"]
-
-console.log(list.map(item => item.length))
-console.log(list.filter(item => item.length > 4))
-console.log(list.reduce((acc, cur) => acc + cur.length, 0))
