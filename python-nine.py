d91746d318261eeaff7b13252a565d3013a86963
Oh seung min <ohmin@gmail.com>
2024-06-30 11:25:48 +0900

    두 문자열 Hello와 world를 공백을 두고 결합하여 출력하는 코드를 작성하는 문제

---
+++ python-nine.py
@@ -0,0 +1,11 @@
+# 문제 9 : 두 문자열 "Hello"와 "World"를 공백을 두고 결합하여 출력하는 코드를 작성하세요.
+
+hi = "hello "
+world = "world"
+result = hi + world
+
+print(result)
+
+other = "hello" + " " + "world"
+
+print(other)
